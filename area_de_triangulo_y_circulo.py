8292cbfa8f93ce7c5372291cb3a542f3512d13d1
Alejandro Garcia <dam1_alu10@inf.ald@a113-pc10.inf.ald>
2026-09-14 14:48:25 +0200

    Área de un triángulo y de un círculo

---
+++ area_de_triangulo_y_circulo.py
@@ -0,0 +1,7 @@
+base = input("Introduce la base del triángulo: ")
+altura = input("Introduce la altura del triángulo: ")
+area = (float(base) * float(altura)) / 2
+print("El área del triángulo es:", area)
+radio = input("Introduce el radio de la circunferencia: ")
+perimetro = 2 * 3.1416 * float(radio)
+print("El perímetro de la circunferencia es:", perimetro)
