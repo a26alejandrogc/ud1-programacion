4db0b00bc8dcb166201d2c9c54427d22787cf65b
a26alejandrogc <a26alejandrogc@iesantonlosada.gal>
2026-09-14 14:30:57 +0200

    Delete division_real_entera_resto.py

--- division_real_entera_resto.py
+++
@@ -1,6 +0,0 @@
-numero1 = int(input("Introduce el primer número: "))
-numero2 = int(input("Introduce el segundo número: "))
-
-print("División:", numero1 / numero2)
-print("División entera:", numero1 // numero2)
-print("Resto:", numero1 % numero2)
