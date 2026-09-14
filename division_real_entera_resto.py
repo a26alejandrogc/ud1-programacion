b342c68daf109ec7a79b01eb50136c072a28db94
Alejandro Garcia <dam1_alu10@inf.ald@a113-pc10.inf.ald>
2026-09-14 14:34:32 +0200

    División real, entera y resto

---
+++ division_real_entera_resto.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Introduce el primer número: "))
+numero2 = int(input("Introduce el segundo número: "))
+
+print("División real:", numero1 / numero2)
+print("División entera:", numero1 // numero2)
+print("Resto:", numero1 % numero2)
