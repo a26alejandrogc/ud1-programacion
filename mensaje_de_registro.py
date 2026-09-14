167ea6283c52252e0938510a347fca0c15b77927
Alejandro Garcia <dam1_alu10@inf.ald@a113-pc10.inf.ald>
2026-09-14 14:41:39 +0200

    Mensaje de registro

---
+++ mensaje_de_registro.py
@@ -0,0 +1,5 @@
+nombre = input("¿Cómo te llamas?" \
++" ")
+apellido1 = input("¿Cuál es tu primer apellido? ")
+apellido2 = input("¿Cuál es tu segundo apellido? ")
+print("El usuario", apellido1, apellido2, ",", nombre, "se ha registrado correctamente.")
